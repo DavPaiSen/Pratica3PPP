78548e4fd72a4fe6b01fd97337fe1518395f0ad7
Davi <davisendin@gmail.com>
2026-06-12 16:35:22 -0300

    ex2 completo

--- ex2/Main.java
+++ ex2/Main.java
@@ -2,7 +2,9 @@ package ex2;
 
 public class Main {
     public static void main(String[] args) {
-        CalculadoraBinaria calculadora = new CalculadoraBinaria();
-        System.out.println(calculadora.somar("1", "01"));
+        Calculadora calculadoraCompleta = new CalculadoraAdapter(new CalculadoraBinaria());
+        System.out.println("1 + 1 = " + calculadoraCompleta.somar(1, 1));
+        System.out.println("1 - 1 = " + calculadoraCompleta.subtrair(1, 1));
+        System.out.println("2 X 2 = " + calculadoraCompleta.multiplicar(2, 2));
     }
 }
